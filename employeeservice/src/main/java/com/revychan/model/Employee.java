961150ca8217436a16dd8a9a57f9b18fcec0255b
marcus1676 <marcuscastellanos@gmail.com>
2020-02-24 21:17:26 -0500

    initializing employee

---
+++ employeeservice/src/main/java/com/revychan/model/Employee.java
@@ -0,0 +1,16 @@
+package com.revychan.model;
+
+/**
+ * Employee
+ */
+public class Employee {
+
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String title;
+    private Department department;
+    private ResourceMetadata rmd;
+    
+}
