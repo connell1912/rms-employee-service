fe521828e43d0cd6e7116a00e71d24d105257e77
aragasa <alexanderagasa@gmail.com>
2020-02-25 09:40:26 -0500

    changed package name to rms

--- employeeservice/src/main/java/com/rms/employeeservice/DemoApplication.java
+++ employeeservice/src/main/java/com/rms/employeeservice/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.revychan.employeeservice;
+package com.rms.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
