4be1856860bd4627fbdf77910cb9c6232098a347
marcus1676 <marcuscastellanos@gmail.com>
2020-02-25 10:59:26 -0500

    2/25 Eployee controllers and dao

---
+++ employeeservice/src/main/java/com/rms/model/Department.java
@@ -0,0 +1,22 @@
+package com.rms.model;
+
+
+/**
+ * This is a department
+ */
+public enum Department {
+
+
+    /**
+     * stored as ints in RDBMS and converted to String type here
+     */
+    TRAINING(1), STAGING(2), QC(3), RETENTINON(4), HR(5);
+
+	private final int type;
+
+	private Department(int type){this.type = type;}
+
+	public int getValue() {
+		return type;
+	}
+}
