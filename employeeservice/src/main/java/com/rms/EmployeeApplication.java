9426e77c448e382df00cbcf43f0b68d8dae7bc61
{connorcaseyc} <{connorcaseyc@gmail.com}>
2020-03-10 10:35:52 -0400

    pushing again

--- employeeservice/src/main/java/com/rms/EmployeeApplication.java
+++ employeeservice/src/main/java/com/rms/EmployeeApplication.java
@@ -8,9 +8,10 @@ import com.rms.model.Role;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
-// @EnableEurekaClient
+@EnableEurekaClient
 @SpringBootApplication
 public class EmployeeApplication {
 
