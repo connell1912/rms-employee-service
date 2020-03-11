c208828b146fbc28ec0c36488972930d52d48ea3
marcus1676 <marcuscastellanos@gmail.com>
2020-03-11 10:38:27 -0400

    added controllerTest. removed pakage-lock.json

--- employeeservice/src/main/java/com/rms/EmployeeApplication.java
+++ employeeservice/src/main/java/com/rms/EmployeeApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
-@EnableEurekaClient
+// @EnableEurekaClient
 @SpringBootApplication
 public class EmployeeApplication {
 
